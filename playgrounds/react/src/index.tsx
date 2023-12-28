c7af242bd5052d822d4067ed7f04e8f7d7324486
Reactongraph <js.developer@ongraph.com>
2023-12-28 15:46:35 +0530

    main package commit

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {Button} from '@designsystem/react';
+import '@designsystem/scss/lib/Button.css';
+
+ReactDOM.render(
+    <Button label="Example Button"/>,
+    document.querySelector('#root')
+)
