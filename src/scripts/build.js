f29394144305cf7eeda8227971a6510b731b216e
Reactongraph <js.developer@ongraph.com>
2023-12-28 15:28:36 +0530

    add button styles

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -1,19 +1,40 @@
-const Fs = require('fs')
-const Path = require('path')
-const Sass = require('node-sass')
+const Fs = require("fs");
+const Path = require("path");
+const Sass = require("node-sass");
 
-const result = Sass.render({
-    data: Fs.readFileSync(
+const getComponents = () => {
+  let allComponents = [];
 
-    ).toString(),
-    outputStyle: 'expanded',
-    outFile: 'global.css',
-    includePaths: [Path.resolve('src')]
+  const type = ["atoms", "molecules", "organisms"];
+
+  type.forEach((type) => {
+    const allFiles = Fs.readdirSync(`src/${type}`).map((file) =>
+    ({
+        input: `src/${type}/${file}`,
+        output:  `lib/${file.slice(0, -4) + 'css'}`
+    })
+     
+    );
+
+    allComponents = [...allComponents, ...allFiles];
+  });
+  return allComponents;
+};
+
+const compile = (path, fileName) => {
+  const result = Sass.renderSync({
+    data: Fs.readFileSync(Path.resolve(path)).toString(),
+    outputStyle: "expanded",
+    outFile: "global.css",
+    includePaths: [Path.resolve("src")],
+  });
+
+  Fs.writeFileSync(Path.resolve(fileName), result.css.toString());
+};
+
+compile("src/global.scss", "lib/global.css");
+
+console.log(getComponents());
+getComponents().forEach(component => {
+    compile(component.input, component.output)
 })
-
-console.log(result.css.toString())
-
-Fs.writeFileSync(
-    Path.resolve('src/lib/global.css'),
-    result.css.toString()
-)
